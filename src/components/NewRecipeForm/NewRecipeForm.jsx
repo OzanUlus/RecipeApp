68077efd1129e9001c70b47615ef2fad8775b138
OzanUlus <ilkerozanulus@gmail.com>
2024-08-27 15:59:40 +0300

    fixed

--- src/components/NewRecipeForm/NewRecipeForm.jsx
+++ src/components/NewRecipeForm/NewRecipeForm.jsx
@@ -4,6 +4,7 @@ import './NewRecipeForm.css'
 export const NewRecipeForm = () => {
   return (
     <form>
+        <h5>Tarif Ekleme</h5>
         <input placeholder='Tarif Adı'  />
         <input placeholder='Görsel Url' />
         <textarea placeholder='Açıklama' rows={3} />
