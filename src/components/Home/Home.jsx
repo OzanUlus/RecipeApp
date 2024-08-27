4948b304ecc19e171b1073f0e4c0bf889d0cefd8
OzanUlus <ilkerozanulus@gmail.com>
2024-08-27 14:20:26 +0300

    home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css"
+
+export const Home = () => {
+  return (
+    <div className='home'>
+        <h3>RecipeApp'a Hoşgeldiniz</h3>
+        <p>Bu uygulamada, en sevdiğiniz yemek tariflerini keşfedecek ve kendi benzersiz yemeklerinizin tariflerini paylaşabileceksiniz. </p>
+        <p>Keyifli vakit geçirmenizi dileriz!</p>
+    </div>
+  )
+}
+export default Home
