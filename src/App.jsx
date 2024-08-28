aff7cabdc4a8bdaa8e696f742a62c71ee9a3cb45
OzanUlus <ilkerozanulus@gmail.com>
2024-08-28 14:30:12 +0300

    first axios

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import { Header } from './components/Header/Header'
 import Home from './components/Home/Home'
 import { RecipeList } from './components/RecipeList/RecipeList'
 import { NewRecipeForm } from './components/NewRecipeForm/NewRecipeForm'
+import axios from 'axios'
 
 function App() {
  const [recipes,setRecipe] = useState([])
 
  useEffect(() =>{
     
+    axios.get("http://localhost:3001/recipes").then((response) =>
+        setRecipe(response.data))
+    .catch(error => {
+        console.log("Thre was an error while fetching the data" , error)
+    } )
     
-    
-    setRecipe(fakeRecipes)
+   
     
  },[])
   
