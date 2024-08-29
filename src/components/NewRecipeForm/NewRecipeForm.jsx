c748a6a94616dc873659200aa9a9cd142bb5e850
OzanUlus <ilkerozanulus@gmail.com>
2024-08-29 18:28:10 +0300

    validation

--- src/components/NewRecipeForm/NewRecipeForm.jsx
+++ src/components/NewRecipeForm/NewRecipeForm.jsx
@@ -6,17 +6,47 @@ export const NewRecipeForm = ({addRecipe}) => {
   const[image, setImage] = useState("")
   const[description, setDescription] = useState("")
 
+  const[errors , setErrors] = useState({
+    title : "",
+    image : "",
+    description : ""
+  })
+
+  const validateForm  = () => {
+     let valid = true
+     const newErrors = {title : "" , image : "" , description : ""}
+
+     if(!title.trim()){
+      newErrors.title = "Başlık boş bırakılmamalıdır."
+      valid = false
+     }
+
+     if(!image.trim()){
+      newErrors.image = "Görsel Url boş bırakılmamalıdır."
+      valid = false
+     }
+
+     if(!description.trim()){
+      newErrors.description = "Açıklama boş bırakılmamalıdır."
+      valid = false
+     }
+
+     setErrors(newErrors)
+     return valid
+  }
+
   const handleSubmit = async (event) => {
 
     event.preventDefault()
      
-    const newRecipe = {title,image,description}
+    if (validateForm()) {
+      const newRecipe = { title, image, description };
 
-    
       await addRecipe(newRecipe);
-      setDescription("")
-      setImage("")
-      setTitle("")
+      setTitle("");
+      setImage("");
+      setDescription("");
+    }
       
 
   }
@@ -28,13 +58,16 @@ export const NewRecipeForm = ({addRecipe}) => {
         <input type='text' 
         value={title} 
         onChange={(event) => setTitle(event.target.value)} placeholder='Tarif Adı'  />
+        {errors.title && <span className="error">{errors.title}</span>}
         <input type='text'
          value={image} 
          onChange={(event) => setImage(event.target.value)} placeholder='Görsel Url' />
+         {errors.title && <span className="error">{errors.image}</span>}
         <textarea 
         type='text' 
         value={description} 
         onChange={(event) => setDescription(event.target.value)} placeholder='Açıklama' rows={3} />
+        {errors.title && <span className="error">{errors.description}</span>}
         <button type='submit' >Kaydet</button>
     </form>
   )
