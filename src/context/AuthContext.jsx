8bc86391f79558eb69e1d365c003b561b36f6307
OzanUlus <ilkerozanulus@gmail.com>
2024-09-01 14:08:30 +0300

    authcontext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import AuthService from "../services/AuthService";
 
 
 export const AuthContex = createContext()
@@ -7,11 +8,32 @@ export const AuthProvider = ({children}) => {
  const [isAuthenticated , setIsAuthenticated] = useState(false)
 
   const login = async(email,password) => {
+    try {
+        const response = await AuthService.loginService(email,password)
+        console.log(response)
+            if(response.access_token){
+                setIsAuthenticated(JSON.parse(localStorage.getItem("user")))
+            }
+        
+    } catch (error) {
+        setIsAuthenticated(false)
+        return new Error(error)
+    }
     
   }
 
+  const logout = () => {
+    AuthService.logoutService()
+    setIsAuthenticated(false)
+  }
+  const value = {
+    isAuthenticated,
+    login,
+    logout
+  }
+
     return (
-        <AuthContex.Provider>
+        <AuthContex.Provider value={value}>
             {children}
         </AuthContex.Provider>
     )
