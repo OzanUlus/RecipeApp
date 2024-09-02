a0578b5df1d37b90b54248ba6b5c031406b7a928
OzanUlus <ilkerozanulus@gmail.com>
2024-09-02 12:50:27 +0300

    getuserinfo

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './Profile.css'
 
 export const Profile = () => {
+  const [user , setUser] = useState({})
+
+  useEffect(() =>{
+
+    const getUserProfile = async() => {
+
+      const response = await axios.get("https://api.escuelajs.co/api/v1/auth/profile",{
+        headers:{
+          "Authorization": `Bearer ${JSON.parse(localStorage.getItem("user")).access_token}` 
+        }
+      }).then(response => {
+        console.log(response);
+        setUser(response.data)
+      })
+    }
+    getUserProfile()
+  },[])
   return (
-    <div>Profile</div>
+    <div className="user-card">
+    <img src={user.avatar} alt={user.name} className="user-avatar" />
+    <div className="user-info">
+      <h2 className="user-name">Ad : {user.name}</h2>
+      <p className="user-email">Role :{user.role}</p>
+      <p className="user-bio">Güncelleme Tarihi :{user.updatedAt}</p>
+    </div>
+  </div>
   )
 }
-export default Profile
+export default Profile 
